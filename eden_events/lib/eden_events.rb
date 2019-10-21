22df180b631108b83dca5212751da70066f726d0
Emil Andreasyan <emil.andreasyan@yahoo.com>
2019-10-21 07:27:57 +0000

    start cli stub

---
+++ eden_events/lib/eden_events.rb
@@ -0,0 +1,7 @@
+require_relative "./eden_events/version"
+require_relative "./eden_events/cli"
+
+module EdenEvents
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ eden_events/lib/eden_events/cli.rb
@@ -0,0 +1,21 @@
+require 'pry'
+
+class EdenEvents::CLI
+  
+  def call
+    puts "welcome to Eden"
+    get_user_month
+  end
+  
+  def get_advertised_months
+    @months = ['July, 2019', 'August, 2019', 'September, 2019', 'October, 2019', ]
+  end
+  
+  def list_months
+    @months.each.with_index do |month, index|
+    #binding.pry
+    puts "#{index + 1} #{month}"
+  end
+  end
+  
+end
