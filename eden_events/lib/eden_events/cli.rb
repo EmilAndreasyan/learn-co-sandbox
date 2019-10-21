30cb9519a236e97d39cd602098a7faef7355f068
Emil Andreasyan <emil.andreasyan@yahoo.com>
2019-10-21 08:12:04 +0000

    add validation check

--- eden_events/lib/eden_events/cli.rb
+++ eden_events/lib/eden_events/cli.rb
@@ -3,18 +3,31 @@ require 'pry'
 class EdenEvents::CLI
   
   def call
-    puts "welcome to Eden"
+    puts "\nwelcome to Eden\n"
+    get_months
+    list_months
     get_user_month
   end
   
-  def get_advertised_months
+  def get_months
     @months = ['July, 2019', 'August, 2019', 'September, 2019', 'October, 2019', ]
   end
   
   def list_months
+    puts "Choose a month to see events"
     @months.each.with_index do |month, index|
-    puts "#{index + 1} #{month}"
+    puts "#{index + 1}. #{month}"
   end
   end
   
+  def get_user_month
+    chosen_month = gets.strip
+    #binding.pry
+    # if valid_input(chosen_month)
+    # end
+  end
+  
+  def valid_input(input, date)
+    input.to_i <= data.length && input.to_i > 0
+  end
 end
